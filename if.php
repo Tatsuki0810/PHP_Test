d868fd65c93847af5e1d513f891cfafc274482bf
Tatsuki Nakamori <55235839+Tatsuki0810@users.noreply.github.com>
2019-11-01 23:53:16 +0900

    Create if.php

---
+++ if.php
@@ -0,0 +1,12 @@
+<?php
+echo ("Only Number or String!\n\n");
+$nt = "\n\t";
+$m = /* fgets(STDIN); */ stst;
+if (is_string($m)) {
+    echo "it's string!";
+} elseif (is_numeric($m)) {
+    echo "it's number!";
+} else {
+    echo $nt;
+}
+?>
